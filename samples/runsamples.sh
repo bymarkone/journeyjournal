08a023a61da4091d7b7c7fbb0a2e030609329380
Marcos Vinicius <k@bymarkone.com>
2015-02-19 01:40:41 -0500

    Adds samples to the application

--- samples/runsamples.sh
+++ samples/runsamples.sh
@@ -2,9 +2,9 @@
 
 echo "Running"
 
-rm twdsl.js
+rm twdsl.jj
 rm data.js
-curl -X POST -H Content-Type:text/plain --data-binary @source.jj localhost:8080/api/jarvis/editor >> twdsl.jj
+curl -X POST -H Content-Type:text/plain --data-binary @jarvis-lang.jj localhost:8080/api/jarvis/editor >> twdsl.jj
 curl -X POST -H Content-Type:text/plain --data-binary @twdsl.jj localhost:8080/api/twdsl/editor
 
 echo "data = {" >> data.js
@@ -14,4 +14,6 @@ echo $',\nfeedback: ' >> data.js && curl -X GET localhost:8080/api/jj/feedback >
 echo $',\nfact: ' >> data.js && curl -X GET localhost:8080/api/jj/fact >> data.js
 
 echo $'\n}' >> data.js
+
+cp data.js ../portal/src/main/resources/public/data.js
 
